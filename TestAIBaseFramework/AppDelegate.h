7194d34775f13542a4f0cab3e6c04b3e9fce6d56
youjian.wu <youjian.wu@qunar.com>
2016-07-18 14:56:27 +0800

    Initial Commit

---
+++ TestAIBaseFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAIBaseFramework
+//
+//  Created by wuyoujian on 16/7/18.
+//  Copyright © 2016年 Asiainfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestAIBaseFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAIBaseFramework
+//
+//  Created by wuyoujian on 16/7/18.
+//  Copyright © 2016年 Asiainfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
